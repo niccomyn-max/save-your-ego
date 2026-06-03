4829a030ee41d9aa6b93ddf62f5de6fbf499a085
Nic Comyn <nic@niccomyn.com>
2026-06-03 04:59:49 +0100

    Exclude API routes from auth proxy

--- proxy.ts
+++ proxy.ts
@@ -9,12 +9,12 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except:
+     * - api routes
      * - _next/static (static files)
-     * - _next/image (image optimization files)
+     * - _next/image (image optimisation files)
      * - favicon.ico (favicon file)
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
